85cfc2a27c7917cfd41e4e14299981f6d328882f
Philipp Marquardt <marquardt@werbeagentur-willers.de>
2025-05-15 12:28:16 +0200

    [FEATURE] Füge Titel, Text, Photo zu Post
    - Migration wurde angepasst und mit
    `$ php artisan migrate` ausgeführt
    - Attribute wurden Post Model hinzugefügt
    - Funktion zum Anlegen der Posts wurde hinzugefügt

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,37 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'posttitle',
+        'postcontent',
+        'postphoto',
+    ];
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'posttitle' => 'required|string|max:255',
+            'postcontent' => 'required',
+            'postphoto' => 'nullable|image|max:2048', // Max 2MB
+        ]);
+
+        $post = new Post();
+        $post->posttitle = $request->posttitle;
+        $post->postcontent = $request->postcontent;
+
+        if ($request->hasFile('postphoto')) {
+            $imagePath = $request->file('postphoto')->store('posts', 'public');
+            $post->image = $imagePath;
+        }
+
+        $post->save();
+    }
 }
