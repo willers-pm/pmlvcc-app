e7abd2eb515ff8e95ee7e8a2f508ad100a94f47a
Philipp Marquardt <marquardt@werbeagentur-willers.de>
2025-05-15 18:05:20 +0200

    [FEATURE] Erstelle Basis-Template
    - Basis-Template wurde angepasst
    - Header-Partial wurde erstellt und eingebunden
    - CSS wurde angepasst

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -13,9 +13,6 @@
         <!-- Favicon -->
 		<link rel="shortcut icon" href="{{ url(asset('favicon.ico')) }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
-
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @livewireStyles
         @livewireScripts
@@ -24,7 +21,10 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
     </head>
 
-    <body>
-        @yield('body')
+    <body class="bg-white">
+        @include('partials.header')
+        <main class="z-2 w-full flex flex-col items-center justify-center min-h-screen">
+            @yield('body')
+        </main>
     </body>
 </html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,7 @@
+@section('title', 'Webseite')
+
+<header class="z-1 sticky top-0 w-full p-2 bg-green-lvc text-[#ffffff] flex items-center justify-center mb-1 text-2xl">
+	<p class="p-2 bg-green-lvc text-[#ffffff] flex items-center justify-center mb-1 text-2xl">
+		@yield('title')
+	</p>
+</header>
